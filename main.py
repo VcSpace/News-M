f6e4e919ccbc4492594125d43dad5deca4c4d4dc
Vcvc <vcvckw@gmail.com>
2020-11-06 21:37:49 +0800

    file_mve

--- main.py
+++ main.py
@@ -2,11 +2,36 @@ from wangyi_fiance import WangYi
 from TongHuaShun import TongHua
 import os, shutil
 import time
+import platform
 
-def filemove():
-    filename = "C:\\Users\\Vcvc\\Desktop\\News_Finance.xlsx"
+def platform():
+    sys = platform.system()
+    if sys == "Windows":
+        return True
+    elif sys == "Linux":
+        return False
 
-    path = "C:\\Users\\Vcvc\\Desktop\\Finance\\"
+def file_move(platform):
+    if platform:
+        win_filemove()
+    else:
+        linux_filemove()
+
+def linux_filemove():
+    path = "./Finance/"
+    filename = "./News_Finance.xlsx"
+    if not path:
+        os.mkdir(path)
+    filetime = time.strftime("%Y_%m_%d_%H_%M", time.localtime())  # year-month-day-hour-minute
+    path2 = path + "闻讯__" + filetime + ".xlsx"
+    shutil.move(filename, path2)
+
+
+def win_filemove():
+    desktop_path = os.path.join(os.path.expanduser('~'),"Desktop") #获取桌面路径
+    filename = desktop_path + "\\News_Finance.xlsx"
+    path = desktop_path +"\\Finance\\"
+
     isExists = os.path.exists(path)
     if not isExists:
         os.mkdir(path)
@@ -15,17 +40,29 @@ def filemove():
     shutil.move(filename, path2)
 
 
-def Wy():
-    Wy = WangYi()
-    Wy.main()
+def Wy(filename):
+    Wy = WangYi(filename)
+    Wy.main(filename)
 
-def THS():
-    Th = TongHua()
-    Th.main()
+def THS(filename):
+    Th = TongHua(filename)
+    Th.main(filename)
+
+def get_News(platform, win_file, lin_file):
+    if platform:
+        Wy(win_file)
+        THS(win_file)
+    else:
+        Wy(lin_file)
+        THS(lin_file)
+
 
 if __name__ == '__main__':
-    Wy() #网易财经
-    THS() #同花顺财经
-    filemove()
-    print("操作完成")
+    desktop_path = os.path.join(os.path.expanduser('~'),"Desktop") #获取桌面路径
+    win_file = desktop_path + "\\News_Finance.xlsx"
+    linux_file = "./News_Finance.xlsx"
 
+    get_News(platform, win_file, linux_file)
+    file_move(platform)
+
+    print("操作完成")
