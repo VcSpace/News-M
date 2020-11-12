1b527c92879ae2f5d6848d92a4b4d36339cc255b
Vcvc <vcvckw@gmail.com>
2020-11-12 23:26:45 +0800

    selg_stock

--- Self_Stock.py
+++ Self_Stock.py
@@ -1,9 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 import xlrd, xlwt
-import xlutils
+from xlutils.copy import copy
 import json
 from requests.cookies import RequestsCookieJar
+import threading
 
 headers = {
     'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36',
@@ -11,8 +12,10 @@ headers = {
 }
 
 class SelfStock(object):
+    threadLock = threading.Lock()
     def __init__(self, file_name):
         self.xlsxname = file_name
+
 
     def Style(self):
         font = xlwt.Font()  # 内容字体
@@ -30,31 +33,88 @@ class SelfStock(object):
         self.style_head.font = font2
         self.style_index.font = font3
 
-    def Deal_Xq_data(self):
-        print(1)
+    num = 3
+    def Deal_Xq_data(self, data, url, name):
+        self.threadLock.acquire()
+
+        xlsxin = xlrd.open_workbook(self.xlsxname, formatting_info=True)
+        sheet = copy(xlsxin)
+        sheet.add_sheet(name)
+        wb = sheet.get_sheet(self.num)
+        self.num = self.num + 1
+
+        data_json = data['data']
+        data_items = data_json['items']
+        data_mk = data_items[0]['market']
+        data_quote = data_items[0]['quote']
+
+        m_status = data_mk['status']
+        stock_code = data_quote['symbol']
+        stock_name = data_quote['name']
+        m_current = data_quote['current']
+        m_percent = data_quote['percent'] #涨跌幅度
+        m_chg = data_quote['chg'] #涨跌价格
+        m_time = data_quote['timestamp']
+        m_open = data_quote['open'] #开盘价
+        m_yesclose = data_quote['last_close'] #昨收
+        m_high = data_quote['high']
+        m_low = data_quote['low']
+        m_avg_price = data_quote['avg_price']
+        m_amount = data_quote['amount'] #成交额
+        m_turnover_rate = data_quote['turnover_rate'] #换手：0.74%
+        m_amplitude = data_quote['amplitude'] #振幅
+        m_market_capital = data_quote['market_capital'] #总市值
+        m_total_shares = data_quote['total_shares'] #总股本
+        m_float_shares = data_quote['float_shares'] #流通股
+        m_high52w = data_quote['high52w'] #52周最高
+        m_low52w = data_quote['low52w'] #52周最低
+        m_limit_up = data_quote['limit_up'] #涨停
+        m_limit_down = data_quote['limit_down'] #跌停
+        m_volume_ratio = data_quote['volume_ratio'] #量比
+        m_pe_ttm = data_quote['pe_ttm'] #市盈率
+        m_pe_forecast = data_quote['pe_forecast'] #动态市盈率
+        m_pe_lyr = data_quote['pe_lyr'] #静态市盈率
+        m_pb = data_quote['pb'] #市净率
+        m_profit = data_quote['profit']
+        m_profit_four = data_quote['profit_four']
+        m_profit_forecast = data_quote['profit_forecast']
+
+        try:
+            sheet.save(self.xlsxname)
+            self.threadLock.release()
+        except Exception:
+            print("Self_Stock Save Error = 1")
+            self.threadLock.release()
 
 
     def get_SelfStock(self):
         url_list = list()
+        name_list = list()
         with open("Code.txt", "r") as f:
             for line in f.readlines():
-                line = line.strip('\n')  # 去掉列表中每一个元素的换行符
+                m_line = line.strip('\n')  # 去掉列表中每一个元素的换行符
                 sep = '#'
                 line = line.split(sep, 1)[0]
                 if line != '':
                     #url = 'https://xueqiu.com/S/{}'.format(line)
                     url = 'https://stock.xueqiu.com/v5/stock/batch/quote.json?extend=detail&is_delay_ft=1&is_delay_hk=0&symbol={}'.format(line)
                     url_list.append(url)
+                    name = m_line.split(sep)[1]
+                    name_list.append(name)
 
         url = 'https://xueqiu.com'
         session = requests.session()
         session.get(url, headers=headers)
+        name_list2 = name_list
         for url in url_list:
-            resp = session.get(url, headers=headers)
-            data = json.loads(resp.text)
-            self.Deal_Xq_data()
-            print(data)
-            #data = requests.get(url, headers=headers)
+            for name in name_list2:
+                name_list2.pop(0)
+                resp = session.get(url, headers=headers)
+                data = json.loads(resp.text)
+                t1 = threading.Thread(target=self.Deal_q_data, args=(data, url, name))
+                t1.start()
+                t1.join()
+                break
 
 
     def main(self, file_name):
