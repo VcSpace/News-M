f7baeca3494a1cc330559dea05ef986a2cd8f07e
Vcvc <vcvckw@gmail.com>
2020-12-08 16:02:15 +0800

    fh

--- src/Fh_Finance.py
+++ src/Fh_Finance.py
@@ -53,30 +53,129 @@ class FengHuang(object):
                 sheet.cell(row=t_row, column=t_col, value=m_title)
                 sheet.cell(row=t_row, column=t_col + 1, value=m_href)
                 t_row = t_row + 1
-
         try:
             wb.save(self.xlsxname)
         except:
             print("FengHuang Save Error = getTopNew")
 
-    """
-    def getNew2(self): #要闻
+
+    def stockNewslist(self, json_str, t_row): #stockNewsList
         wb = load_workbook(self.xlsxname)
         sheet = wb.get_sheet_by_name("Fh")
-        t_row = sheet.max_row + 1
+        Newslist = json_str['stockNewsList']
         t_col = 1
-        sheet.cell(row=t_row, column=t_col, value="财经要闻")
+        temp = 0
+        for m_new in Newslist:
+            temp = temp + 1
+            if temp == 6:
+                temp = 0
+                t_row = t_row + 1
+
+            else:
+                m_title = m_new['title']
+                m_time = m_new['newsTime']
+                m_href = m_new['url']
+                sheet.cell(row=t_row, column=t_col, value=m_title)
+                sheet.cell(row=t_row, column=t_col + 1, value=m_href)
+                sheet.cell(row=t_row, column=t_col + 3, value=m_time)
+                t_row = t_row + 1
+        try:
+            wb.save(self.xlsxname)
+        except:
+            print("FengHuang Save Error = getNewsList")
+
+
+    def CompanyNews(self, json_str): #stockNewsList 没有top  只有top下的5条
+        wb = load_workbook(self.xlsxname)
+        sheet = wb.get_sheet_by_name("Fh")
+        t_row = sheet.max_row + 2
+        t_col = 1
+        sheet.cell(row=t_row, column=t_col, value="公司要闻")
         t_row = t_row + 1
         sheet.cell(row=t_row, column=t_col, value="新闻标题")
         sheet.cell(row=t_row, column=t_col + 1, value="新闻链接")
         sheet.cell(row=t_row, column=t_col + 2, value="新闻简介")
-        sheet.cell(row=t_row, column=t_col + 2, value="新闻时间")
+        sheet.cell(row=t_row, column=t_col + 3, value="新闻时间")
         t_row = t_row + 1
 
-        #datalist = self.soup.find_all(class_='list-1D1mq9N- clearfix')
-        datalist = self.soup.find_all(class_='text-2iuwVuE4')
-        print(datalist)
-    """
+        CNews = json_str['news']
+        m_CNews = json_str['newsList']
+        for m_new in CNews:
+            m_href = m_new['url']
+            m_title = m_new['title']
+            m_href = m_href.replace("//", "https://")
+            sheet.cell(row=t_row, column=t_col, value=m_title)
+            sheet.cell(row=t_row, column=t_col + 1, value=m_href)
+            t_row = t_row + 1
+            break
+
+        for m_new in m_CNews:
+            m_time = m_new['newsTime']
+            m_href = m_new['url']
+            m_title = m_new['title']
+            sheet.cell(row=t_row, column=t_col, value=m_title)
+            sheet.cell(row=t_row, column=t_col + 1, value=m_href)
+            sheet.cell(row=t_row, column=t_col + 2, value=m_time)
+            t_row = t_row + 1
+        try:
+            wb.save(self.xlsxname)
+        except:
+            print("FengHuang Save Error = CompanyNews2")
+
+    def marketAnalysis(self, json_str):
+        wb = load_workbook(self.xlsxname)
+        sheet = wb.get_sheet_by_name("Fh")
+        t_row = sheet.max_row + 2
+        t_col = 1
+        sheet.cell(row=t_row, column=t_col, value="操盘分析")
+        t_row = t_row + 1
+        sheet.cell(row=t_row, column=t_col, value="新闻标题")
+        sheet.cell(row=t_row, column=t_col + 1, value="新闻链接")
+        sheet.cell(row=t_row, column=t_col + 2, value="新闻简介")
+        sheet.cell(row=t_row, column=t_col + 3, value="新闻时间")
+        t_row = t_row + 1
+
+        Newslist = json_str['marketAnalysis']
+        for m_news in Newslist:
+            m_href = m_news['url']
+            m_title = m_news['title']
+            m_href = m_href.replace("//", "https://")
+            sheet.cell(row=t_row, column=t_col, value=m_title)
+            sheet.cell(row=t_row, column=t_col + 1, value=m_href)
+            t_row = t_row + 1
+        try:
+            wb.save(self.xlsxname)
+        except:
+            print("FengHuang Save Error = marketAnalysis")
+
+
+    def IPOObservation(self, json_str): #stockNewsList 没有top  只有top下的5条
+        wb = load_workbook(self.xlsxname)
+        sheet = wb.get_sheet_by_name("Fh")
+        t_row = sheet.max_row + 2
+        t_col = 1
+
+        sheet.cell(row=t_row, column=t_col, value="IPO观察")
+        t_row = t_row + 1
+        sheet.cell(row=t_row, column=t_col, value="新闻标题")
+        sheet.cell(row=t_row, column=t_col + 1, value="新闻链接")
+        sheet.cell(row=t_row, column=t_col + 2, value="新闻简介")
+        sheet.cell(row=t_row, column=t_col + 3, value="新闻时间")
+        t_row = t_row + 1
+
+        NewsList = json_str['hotPlate']
+        for m_new in NewsList:
+            m_href = m_new['url']
+            m_title = m_new['title']
+            m_href = m_href.replace("//", "https://")
+            sheet.cell(row=t_row, column=t_col, value=m_title)
+            sheet.cell(row=t_row, column=t_col + 1, value=m_href)
+            t_row = t_row + 1
+        try:
+            wb.save(self.xlsxname)
+        except:
+            print("FengHuang Save Error = IPOObservation")
+
 
     def getStockNews(self):
         wb = load_workbook(self.xlsxname)
@@ -98,12 +197,12 @@ class FengHuang(object):
         data = data.replace(";", "")
         json_str = json.loads(data)
         print(data)
-        stockNews = json_str['stockNews']
+        m_row = t_row + 1
+        stockNews = json_str['stockNews'] #top新闻
         for m_new in stockNews:
             m_title = m_new['title']
             href = m_new['url']
             m_href = href.replace("//", "https://")
-
             sheet.cell(row=t_row, column=t_col, value=m_title)
             sheet.cell(row=t_row, column=t_col + 1, value=m_href)
             t_row = t_row + 1
@@ -113,6 +212,10 @@ class FengHuang(object):
         except:
             print("FengHuang Save Error = getStockNews")
 
+        self.stockNewslist(json_str, m_row) #stockNewsList 没有top  只有top下的5条
+        self.CompanyNews(json_str) #stockNewsList 没有top  只有top下的5条
+        self.marketAnalysis(json_str)
+        self.IPOObservation(json_str) #stockNewsList 没有top  只有top下的5条
 
     def main(self, filename):
         Fh = FengHuang(filename)
@@ -120,18 +223,3 @@ class FengHuang(object):
         Fh.request()
         Fh.getTopNew()
         Fh.getStockNews()
-"""
-stockNews
-stockNewsList
-news
-newsList
-bannerPic
-marketAnalysis
-hotPlate
-gnin
-gnout
-track
-fiveDaysBuy
-fiveDaysSell
-logs
-"""
