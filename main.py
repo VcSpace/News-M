9f3e606bd95caaefa76afd1957c275c165a1f36e
Vcvc <vcvckw@gmail.com>
2021-02-09 01:07:13 +0800

    init

--- main.py
+++ main.py
@@ -5,24 +5,19 @@ from src.Ths_Finance import Ths
 from src.Jrj_Finance import Jrj
 from src.Fh_Finance import Fh
 from src.East_Finance import Ew
-from src.Self_Stock import Stock
 from src.CCTV_News import CCTV
 from src.Sina_Finance import Sina
-
 
 def get_News(platform, filename):
     Wy.main(filename)
     t1 = threading.Thread(target=CCTV.main, args=())
-    t2 = threading.Thread(target=Stock.main, args=(filename,))
     t1.start()
-    t2.start()
     Ths.main(filename)
     Jrj.main(filename)
     Fh.main(filename)
     Ew.main(filename)
     Sina.main(filename)
     t1.join()
-    t2.join()
 
 def get_filename(platform):
     if m_platform == True:
