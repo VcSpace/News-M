86f33579cd92649a6950a1de4f1bfe66e39ed6fa
Vcvc <vcvckw@gmail.com>
2021-08-05 11:55:32 +0800

    init

--- main.py
+++ main.py
@@ -5,22 +5,33 @@ from src.Ths_Finance import Ths
 from src.Jrj_Finance import Jrj
 from src.Fh_Finance import Fh
 from src.East_Finance import Ew
+from src.Self_Stock import Stock
 from src.CCTV_News import CCTV
 from src.Sina_Finance import Sina
 from src.Xhs_Finance import Xhs
+from src.Yh_Finance import Yh
+from src.Sg_Finance import Sg
 
 
-def get_News(platform, filename):
+def get_News(platform, filename, debug):
+    #debug True开启
+    if debug:
+        Wy.create_file(filename)
+        return
     Wy.main(filename)
-    Xhs.main(filename)
     t1 = threading.Thread(target=CCTV.main, args=())
+    t2 = threading.Thread(target=Stock.main, args=(filename,))
     t1.start()
+    t2.start()
+    Xhs.main(filename)
     Ths.main(filename)
     Jrj.main(filename)
     Fh.main(filename)
     Ew.main(filename)
     Sina.main(filename)
+    Sg.main(filename)
     t1.join()
+    t2.join()
 
 def get_filename(platform):
     if m_platform == True:
@@ -31,9 +42,10 @@ def get_filename(platform):
         return linux_file
 
 if __name__ == '__main__':
+    debug = False
     m_platform = pt.get_platform() #判断系统
-    filename = get_filename(m_platform) #设置文件名
-    get_News(m_platform, filename) #获取信息
+    filename = get_filename(m_platform)
+    get_News(m_platform, filename, debug) #获取信息
 
     pt.file_move(m_platform) #文件移动 重命名操作
 
