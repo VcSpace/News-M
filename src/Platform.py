40712ee61de054bc7c59f0f6fc8eb4cbb60de850
Vcvc <vcvckw@gmail.com>
2021-08-09 22:33:45 +0800

    pt

--- src/Platform.py
+++ src/Platform.py
@@ -103,5 +103,50 @@ class Files(object):
         if not dir:
             os.mkdir(dir)
     """
+    def getwinfile(self):
+        desktop_path = os.path.join(os.path.expanduser('~'), "Desktop")  # 获取桌面路径
+        filename = desktop_path + "\\Finance\\News_Finance.xlsx"
+        path = desktop_path + "\\Finance\\News\\"
+        # filetime = time.strftime("%Y_%m_%d_%H_%M", time.localtime()) #year-month-day-hour-minute
+        filetime = time.strftime("%Y_%m_%d", time.localtime())  # year-month-day-hour-minute
+        path2 = path + "闻讯__" + filetime + ".xlsx"
+        return path2
+
+    def getlinfile(self):
+        path = "./Finance/News/"
+        filename = "./News_Finance.xlsx"
+        isExists = os.path.exists(path)
+        # filetime = time.strftime("%Y_%m_%d_%H_%M", time.localtime())  # year-month-day-hour-minute
+        filetime = time.strftime("%Y_%m_%d", time.localtime())  # year-month-day-hour-minute
+        path2 = path + "闻讯__" + filetime + ".xlsx"
+        return path2
+
+
+    def getfilename(self):
+        if self.get_platform() == True:
+            return self.getwinfile()
+        else:
+            return self.getlinfile()
+
+    def filename(self):
+        filetime = time.strftime("%Y_%m_%d", time.localtime())  # year-month-day-hour-minute
+        path2 = "闻讯__" + filetime + ".xlsx"
+        return path2
+
+    def getwinpath(self):
+        desktop_path = os.path.join(os.path.expanduser('~'), "Desktop")  # 获取桌面路径
+        path = desktop_path + "\\Finance\\" + " \\"
+        return path
+
+    def getlinpath(self):
+        path = "./Finance/" + " /"
+        return path
+
+    def getpath(self):
+        if self.get_platform() == True:
+            return self.getwinpath()
+        else:
+            return self.getlinpath()
+
 
 pt = Files()
