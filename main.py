9d393cb768dc38296c29dcd25fe4dbee3c9f9f69
Vcvc <vcvckw@gmail.com>
2021-08-09 22:32:02 +0800

    init

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from src.Ths_Finance import Ths
 from src.Jrj_Finance import Jrj
 from src.Fh_Finance import Fh
 from src.East_Finance import Ew
+from src.Self_Stock import Stock
 from src.CCTV_News import CCTV
 from src.Sina_Finance import Sina
 from src.Xhs_Finance import Xhs
+from src.Yh_Finance import Yh
 from src.Sg_Finance import Sg
-
+from src.Baidu_upload import Bd
 
 def get_News(platform, filename, debug):
     #debug True开启
@@ -18,7 +20,9 @@ def get_News(platform, filename, debug):
         return
     Wy.main(filename)
     t1 = threading.Thread(target=CCTV.main, args=())
-    t1.start()
+    t2 = threading.Thread(target=Stock.main, args=(filename,))
+    #t1.start()
+    t2.start()
     Xhs.main(filename)
     Ths.main(filename)
     Jrj.main(filename)
@@ -26,7 +30,8 @@ def get_News(platform, filename, debug):
     Ew.main(filename)
     Sina.main(filename)
     Sg.main(filename)
-    t1.join()
+    #t1.join()
+    t2.join()
 
 def get_filename(platform):
     if m_platform == True:
@@ -43,6 +48,19 @@ if __name__ == '__main__':
     get_News(m_platform, filename, debug) #获取信息
 
     pt.file_move(m_platform) #文件移动 重命名操作
+
+    """
+    授权步骤
+    命令行bypy info || python -m bypy info || python3 -m bypy info
+    打开链接-登陆-授权
+    复制授权码
+    粘贴到命令行-enter
+    """
+    bd_flag = True
+    if(bd_flag == False):
+        print("如果需要上传到云盘备份 请自行开启bd.main \n")
+    else:
+        Bd.main()
 
     print("操作完成")
 
