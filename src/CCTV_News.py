ffdd0cf8c5667884ce720ba702e983933b413f9c
Vcvc <vcvckw@gmail.com>
2022-04-06 18:19:36 +0800

    Update CCTV_News.py

--- src/CCTV_News.py
+++ src/CCTV_News.py
@@ -5,12 +5,6 @@ import shutil
 from src.Platform import pt
 import time
 
-"""
-http://mrxwlb.com/category/mrxwlb-text/amp/
-cn.govopendata.com
-http://www.11417.cn/cctv.html
-"""
-
 headers = {
     'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36',
 }
